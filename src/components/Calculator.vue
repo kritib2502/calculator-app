c3fc23514a989d2d21bb7d7874cdb9ee145ade04
sara-rahmani <sararahmani96@gmail.com>
2023-03-23 14:41:48 -0700

    done

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,43 +1,151 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
+
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+<div class="container">
+  <div class="calculator_container">
+    <div class="display">
+{{ current || '0' }}
+    </div>
+<div class="keyboard">
+<div class="row">
+  <button @click ="clearAllDisplay()">AC</button>
+  <button @click ="clearDispay()" >C</button>
+  <button v-on:click ="memoryClear()" >MC</button>
+  <button  v-on:click ="memorySave()">MS</button>
+  <button  v-on:click ="handleOperator('×')">×</button>
+</div>
+<div class="row">
+  <button  v-on:click ="appendSymbol('7')">7</button>
+  <button  v-on:click ="appendSymbol('8')">8</button>
+  <button  v-on:click ="appendSymbol('9')">9</button>
+  <button  v-on:click ="memoryRecall()">MR</button>
+  <button  v-on:click ="handleOperator('÷')">÷</button>
+</div><div class="row">
+  <button  v-on:click ="appendSymbol('4')">4</button>
+  <button  v-on:click ="appendSymbol('5')">5</button>
+  <button  v-on:click ="appendSymbol('6')">6</button>
+  <button  v-on:click ="memoryMinus()">M-</button>
+  <button  v-on:click ="handleOperator('-')">-</button>
+</div><div class="row">
+  <button  v-on:click ="appendSymbol('1')">1</button>
+  <button  v-on:click ="appendSymbol('2')">2</button>
+  <button  v-on:click ="appendSymbol('3')">3</button>
+  <button  v-on:click ="memoryPlus()">M+</button>
+  <button  v-on:click ="handleOperator('+')">+</button>
+</div><div class="row">
+  <button  v-on:click ="convertDecimal()">.</button>
+  <button  v-on:click ="appendSymbol('0')">0</button>
+  <button  v-on:click ="changeSign()">+/-</button>
+  <button  v-on:click ="square()">&#8730</button>
+
+  <button  v-on:click ="convertPercent()">%</button>
+</div>
+<button  v-on:click ="handleOperator('=')">=</button>
+
+</div>
+
+
   </div>
+</div>
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
+<script>
+export default {
+  data () {
+    return {
+      current: '',
+      previous: null,
+      operator: null,
+      clickedOperator: false,
+      memory:''
+    }
+  },
+  methods: {
+    square(){
+      if(this.current > 0){
+        this.current = Math.sqrt(this.current)
+      }
+else{
+  this.current='Error'
 }
+    },
+    memoryClear(){
+      this.memory=''
+    },
+    memorySave(){
+      this.memory=this.current
+    },
+    memoryRecall(){
+      this.current=this.memory
+    },
+    memoryMinus(){
+      this.current=this.memory - this.current
+    },
+    memoryPlus(){
+      this.current = parseFloat(this.memory) + parseFloat(this.current);
+    },
+    clearAllDisplay () {
+      this.current = ''
+      this.previous = null
+      this.operator = null
+      this.clickedOperator = false
+    },
+    clearDisplay () {
+      this.current.slice(0,-1);
+     
+    },
+    appendSymbol (number) {
+      if (this.clickedOperator) {
+        this.clickedOperator = false
+        this.current = number
+      } else {
+        this.current = this.current === '0' ? number : `${this.current}${number}`
+      }
+    },
+    convertDecimal () {
+      if (this.clickedOperator) {
+        this.current = '0.'
+        this.clickedOperator = false
+      }
+      if (this.current.indexOf('.') === -1) {
+        this.current += '0.'
+      }
+    },
+    changeSign () {
+      this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : `-${this.current}`
+    },
+    convertPercent () {
+      this.current = `${parseFloat(this.current) / 100}`
+    },
+    handleOperator (newOperator) {
+      const numberValue = parseFloat(this.current)
+      if (this.operator && this.clickedOperator) {
+        this.operator = newOperator
+      }
+      if (this.previous == null) {
+        this.previous = numberValue
+      } else if (this.operator) {
+        const result = this.calculate(this.previous, numberValue, this.operator)
+        this.current = String(result)
+        this.previous = result
+      }
+      this.clickedOperator = true
+      this.operator = newOperator
+    },
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+    calculate (first, second, operator) {
+      switch (operator) {
+        case '+':
+          return first + second
+        case '-':
+          return first - second
+        case '×':
+          return first * second
+        case '÷':
+          return first / second
+        default:
+          return second
+      }
+    }
   }
 }
-</style>
+</script>
